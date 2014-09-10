06d93e52669ee1e6a7daed37e4494c74beceeb6d
wheaton <haynes@neuralanalytics.com>
2014-09-09 20:57:43 -0700

    test directory structure

---
+++ src/main/java/test/beastmachine/ggplot/GGPlotTest.java
@@ -0,0 +1,14 @@
+package test.beastmachine.ggplot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GGPlotTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
