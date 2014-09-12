1884294477308984017797d7fa613e7132df4ec8
wheaton5 <whheaton@gmail.com>
2014-09-11 18:59:27 -0700

    comments and javadocs for DataFrame

--- src/main/java/org/beastmachine/dataframe/DataFrame.java
+++ src/main/java/org/beastmachine/dataframe/DataFrame.java
@@ -5,27 +5,43 @@ import gnu.trove.list.array.TDoubleArrayList;
 import gnu.trove.list.array.TFloatArrayList;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.list.array.TLongArrayList;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 import javax.xml.ws.Holder;
 
-import com.google.common.base.Preconditions;
-
+/**
+ * 
+ * @author wheaton5
+ *
+ */
 public class DataFrame {
 
-	private List<Column> columnList;
 	private int nRows;
 	private HashMap<String, Column> columnsByName;
 	
+	/**
+	 * DataFrame object that mimics R's DataFrame data structure
+	 * at least for the purposes of plotting a ggplot style plot
+	 */
 	public DataFrame(){
 		nRows = 0;
-		columnList = new ArrayList<Column>();
 		columnsByName = new HashMap<String, Column>();
 	}
 
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and 1 or more Strings or an array of Strings for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a factor because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return
+	 */
 	public DataFrame c(String colName, String ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -33,6 +49,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a list of Strings for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a factor because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return
+	 */
 	public DataFrame c(String colName, List<String> colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -40,6 +69,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more ints or an array of ints for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return
+	 */
 	public DataFrame c(String colName, int ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -47,6 +89,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Integers or an array of Integers for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Integer ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -54,6 +109,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a trove TIntArrayList of ints for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, TIntArrayList colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -61,6 +129,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more bytes or an array of bytes for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, byte ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -68,6 +149,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Bytes or an array of Bytes for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Byte ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -75,6 +169,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a trove TByteArrayList of bytes for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, TByteArrayList colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -83,7 +190,19 @@ public class DataFrame {
 	}
 	
 	
-	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more floats or an array of floats
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, float ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -91,6 +210,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Floats or an array of Floats
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Float ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -98,6 +230,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a trove TFloatArrayList of floats for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, TFloatArrayList colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -105,6 +250,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more doubles or an array of doubles for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, double ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -112,6 +270,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Doubles or an array of Doubles for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Double ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -119,6 +290,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a trove TDoubleArrayList of doubles for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, TDoubleArrayList colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -126,6 +310,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more longs or an array of longs for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, long ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -133,6 +330,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Longs or an array of Longs for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Long ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -140,6 +350,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and a trove TLongArrayList of longs for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a numeric because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, TLongArrayList colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -147,6 +370,19 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more booleans or an array of booleans for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a factor because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, boolean ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
@@ -154,12 +390,24 @@ public class DataFrame {
 		return this;
 	}
 	
+	/**
+	 * Adds a column to the DataFrame and returns that modified DataFrame
+	 * for easy in-line column addition like new DataFrame().c("name",array1).c("name2",array2)
+	 * 
+	 * Takes a column name and one or more Booleans or an array of Booleans for
+	 * the column data and returns the same DataFrame modified
+	 * 
+	 * This column will be treated as a factor because of its type
+	 * 
+	 * @param colName
+	 * @param colData
+	 * @return 
+	 */
 	public DataFrame c(String colName, Boolean ... colData){
 		Holder<Integer> rowHolder = new Holder<Integer>(nRows);
 		Column col = Column.getInstance(colData, rowHolder);
 		columnsByName.put(colName, col);
 		return this;
 	}
-	
 	
 }
