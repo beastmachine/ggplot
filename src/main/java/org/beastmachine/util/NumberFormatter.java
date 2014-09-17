ff2ab22bcb56d13c130004d312f0b56efd1f8ed2
Peter <Peter@neuralanalytics.com>
2014-09-17 12:31:19 -0400

    Cleaned up number formatter

--- src/main/java/org/beastmachine/util/NumberFormatter.java
+++ src/main/java/org/beastmachine/util/NumberFormatter.java
@@ -7,15 +7,10 @@ public class NumberFormatter {
 
   public static String format(double d) {
     String naive = String.format("%f",d);
-    int decimal = naive.length();
-    for (int ii=0; ii<naive.length(); ii++) {
-      char c = naive.charAt(ii);
-      if (c == '.') {
-        decimal = ii;
-        break;
-      }
-    }
-    
+    System.out.println(naive);
+    int decimal = naive.indexOf(".");
+    if (decimal == -1) { decimal = naive.length(); }
+
     int firstZero = -1;
     int zeroCount = 0;
     for (int ii=0; ii<naive.length(); ii++) {
@@ -38,6 +33,17 @@ public class NumberFormatter {
         firstZero = -1;
       }
     }
+    if (zeroCount >= ZEROS_LIMIT) {
+      naive = naive.substring(0,max(firstZero,decimal));
+      if (naive.endsWith(".")) {
+        naive = naive.substring(0,naive.length()-1);
+      }
+      return naive;
+    }
     return naive;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(format(1));
   }
 }
