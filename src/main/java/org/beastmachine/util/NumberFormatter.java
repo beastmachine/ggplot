4b9ee8a5e5a38eaca6281be1ced69bee6de0bcf0
Peter <Peter@neuralanalytics.com>
2014-09-17 12:07:45 -0400

    Added sensible number formatter

---
+++ src/main/java/org/beastmachine/util/NumberFormatter.java
@@ -0,0 +1,47 @@
+package org.beastmachine.util;
+
+import static java.lang.Math.max;
+
+public class NumberFormatter {
+  private static final int ZEROS_LIMIT=5;
+
+  public static String format(double d) {
+    String naive = String.format("%f",d);
+    int decimal = naive.length();
+    for (int ii=0; ii<naive.length(); ii++) {
+      char c = naive.charAt(ii);
+      if (c == '.') {
+        decimal = ii;
+        break;
+      }
+    }
+    
+    int firstZero = -1;
+    int zeroCount = 0;
+    for (int ii=0; ii<naive.length(); ii++) {
+      char c = naive.charAt(ii);
+      if (c == '.') {
+        continue;
+      } else if (c=='0') {
+        if (zeroCount==0) {
+          firstZero = ii;
+        }
+        zeroCount++;
+      } else if (zeroCount >= ZEROS_LIMIT) {
+        naive = naive.substring(0,max(firstZero,decimal));
+        if (naive.endsWith(".")) {
+          naive = naive.substring(0,naive.length()-1);
+        }
+        return naive;
+      } else {
+        zeroCount = 0;
+        firstZero = -1;
+      }
+    }
+    return naive;
+  }
+  
+  public static void main(String[] args) {
+    System.out.println(format(200000001));
+  }
+}
