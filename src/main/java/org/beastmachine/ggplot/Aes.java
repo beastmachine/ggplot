eb812729a9b465354b828d060c72d08f9805633e
wheaton5 <whheaton@gmail.com>
2014-10-01 20:17:05 -0400

    changed aes syntax to aes().x("varname") etc

--- src/main/java/org/beastmachine/ggplot/Aes.java
+++ src/main/java/org/beastmachine/ggplot/Aes.java
@@ -12,10 +12,220 @@ public class Aes {
   public Aes() {
     mapping = new TreeMap<Aesthetic, String>();
   }
-
-  public Aes a(Aesthetic key, String val) {
+  
+  private Aes a(Aesthetic key, String val) {
     mapping.put(key, val);
     return this;
+  }
+  
+  public Aes adj(String val){
+  	this.a(Aesthetic.adj, val);
+  	return this;
+  }
+  
+  public Aes alpha(String val){
+  	this.a(Aesthetic.alpha, val);
+  	return this;
+  }
+  
+  public Aes angle(String val){
+  	this.a(Aesthetic.angle, val);
+  	return this;
+  }
+  
+  public Aes bg(String val){
+  	this.a(Aesthetic.bg, val);
+  	return this;
+  }
+  
+  public Aes cex(String val){
+  	this.a(Aesthetic.cex, val);
+  	return this;
+  }
+  
+  public Aes col(String val){
+  	this.a(Aesthetic.col, val);
+  	return this;
+  }
+  
+  public Aes color(String val){
+  	this.a(Aesthetic.color, val);
+  	return this;
+  }
+  
+  public Aes colour(String val){
+  	this.a(Aesthetic.colour, val);
+  	return this;
+  }
+  
+  public Aes fg(String val){
+  	this.a(Aesthetic.fg, val);
+  	return this;
+  }
+  
+  public Aes fill(String val){
+  	this.a(Aesthetic.fill, val);
+  	return this;
+  }
+  
+  public Aes group(String val){
+  	this.a(Aesthetic.group, val);
+  	return this;
+  }
+  
+  public Aes hjust(String val){
+  	this.a(Aesthetic.hjust, val);
+  	return this;
+  }
+  
+  public Aes label(String val){
+  	this.a(Aesthetic.label, val);
+  	return this;
+  }
+  
+  public Aes linetype(String val){
+  	this.a(Aesthetic.linetype, val);
+  	return this;
+  }
+  
+  public Aes lower(String val){
+  	this.a(Aesthetic.lower, val);
+  	return this;
+  }
+  
+  public Aes lty(String val){
+  	this.a(Aesthetic.lty, val);
+  	return this;
+  }
+  
+  public Aes lwd(String val){
+  	this.a(Aesthetic.lwd, val);
+  	return this;
+  }
+  
+  public Aes max(String val){
+  	this.a(Aesthetic.max, val);
+  	return this;
+  }
+  
+  public Aes middle(String val){
+  	this.a(Aesthetic.middle, val);
+  	return this;
+  }
+  
+  public Aes min(String val){
+  	this.a(Aesthetic.min, val);
+  	return this;
+  }
+  
+  public Aes order(String val){
+  	this.a(Aesthetic.order, val);
+  	return this;
+  }
+  
+  public Aes pch(String val){
+  	this.a(Aesthetic.pch, val);
+  	return this;
+  }
+  
+  public Aes radius(String val){
+  	this.a(Aesthetic.radius, val);
+  	return this;
+  }
+  
+  public Aes sample(String val){
+  	this.a(Aesthetic.sample, val);
+  	return this;
+  }
+  
+  public Aes shape(String val){
+  	this.a(Aesthetic.shape, val);
+  	return this;
+  }
+  
+  public Aes size(String val){
+  	this.a(Aesthetic.size, val);
+  	return this;
+  }
+  
+  public Aes srt(String val){
+  	this.a(Aesthetic.srt, val);
+  	return this;
+  }
+  
+  public Aes upper(String val){
+  	this.a(Aesthetic.upper, val);
+  	return this;
+  }
+  
+  public Aes vjust(String val){
+  	this.a(Aesthetic.vjust, val);
+  	return this;
+  }
+  
+  public Aes weight(String val){
+  	this.a(Aesthetic.weight, val);
+  	return this;
+  }
+  
+  public Aes width(String val){
+  	this.a(Aesthetic.width, val);
+  	return this;
+  }
+  
+  public Aes x(String val){
+  	this.a(Aesthetic.x, val);
+  	return this;
+  }
+  
+  public Aes xend(String val){
+  	this.a(Aesthetic.xend, val);
+  	return this;
+  }
+  
+  public Aes xmax(String val){
+  	this.a(Aesthetic.xmax, val);
+  	return this;
+  }
+  
+  public Aes xmin(String val){
+  	this.a(Aesthetic.xmin, val);
+  	return this;
+  }
+  
+  public Aes xintercept(String val){
+  	this.a(Aesthetic.xintercept, val);
+  	return this;
+  }
+  
+  public Aes y(String val){
+  	this.a(Aesthetic.y, val);
+  	return this;
+  }
+  
+  public Aes yend(String val){
+  	this.a(Aesthetic.yend, val);
+  	return this;
+  }
+  
+  public Aes ymax(String val){
+  	this.a(Aesthetic.ymax, val);
+  	return this;
+  }
+  
+  public Aes ymin(String val){
+  	this.a(Aesthetic.ymin, val);
+  	return this;
+  }
+  
+  public Aes yintercept(String val){
+  	this.a(Aesthetic.yintercept, val);
+  	return this;
+  }
+  
+  public Aes z(String val){
+  	this.a(Aesthetic.z, val);
+  	return this;
   }
   
   public Set<Aesthetic> getSetAesthetics(){
--- src/test/java/org/beastmachine/ggplot/GGplotTest.java
+++ src/test/java/org/beastmachine/ggplot/GGplotTest.java
@@ -3,7 +3,6 @@ package org.beastmachine.ggplot;
 import java.io.File;
 import java.util.ArrayList;
 
-import org.beastmachine.ggplot.GGPlot;
 import org.junit.Test;
 
 import static org.beastmachine.ggplot.GGPlot.*;
@@ -15,8 +14,8 @@ public class GGplotTest {
 		int[] v1 = {1,2,3,4};
 		int[] v2 = {1,2,3,4};
 		System.out.println("current corking directory "+new File("./").getAbsolutePath());
-		ggplot(dataframe().c("x",v1).c("y",v2),aes(x,"x",y,"y")).geom_point().ggsave("./src/main/resources/ggplot.pdf");
-	//ggplot(dataframe().c("x",x).c("y",y),aes().x("x").y("y")).geom_point().ggsave("/Users/wheaton/ggplot.pdf") 
+		GGPlot g = ggplot(dataframe().c("x",v1).c("y",v2),aes().x("x").y("y")).geom_point();
+		ggsave(g, "./src/main/resources/ggplot.pdf");
  // ggsave(ggplot(data.frame(x=x,y=y),aes(x,y))+geom_point(),"/Users/wheaton/ggplot.pdf")     
 	}
 	
